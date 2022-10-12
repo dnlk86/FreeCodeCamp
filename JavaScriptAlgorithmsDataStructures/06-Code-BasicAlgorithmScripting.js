6da94733177736a522b4da42acfafd2d9d59e049
Daniel Keicher <dnlkch86@gmail.com>
2022-10-12 09:48:37 +0200

    added more functions

--- JavaScriptAlgorithmsDataStructures/06-Code-BasicAlgorithmScripting.js
+++ JavaScriptAlgorithmsDataStructures/06-Code-BasicAlgorithmScripting.js
@@ -4,6 +4,8 @@ function convertCtoF(celsius) {
     return fahrenheit;
 }
 
+convertCtoF(30);
+
 // REVERSE A STRING
 function reverseString(str) {
     let res = ''
@@ -13,6 +15,8 @@ function reverseString(str) {
     return res;
 }
 
+reverseString("hello");
+
 // FACTORIALIZE A NUMBER
 function factorialize(num) {
     if (num === 0) {
@@ -21,3 +25,81 @@ function factorialize(num) {
       return num * factorialize(num - 1);
     }
   }
+
+  factorialize(5);
+
+// FIND THE LONGEST WORD IN A STRING
+function findLongestWordLength(str) {
+    let max = 0;
+    const arr = str.split(" ");
+    for (let i in arr) {
+      max = arr[i].length > max ? arr[i].length : max;
+    }
+    return max;
+  }
+
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+// RETURN LARGEST NUMBERS IN ARRAYS
+function largestOfFour(arr) {
+    let newArr = [];
+    for (let i in arr) {
+      newArr.push(Math.max(...arr[i]));
+    }
+    return newArr;
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// CONFIRM THE EDITING
+function confirmEnding(str, target) {
+    return str.substr(-target.length) === target ? true : false; // WITHOUT USING THE ENDSWITH METHOD
+  }
+  
+  confirmEnding("Bastian", "n");
+
+// REPEAT A STRING REPEAT A STRING
+function repeatStringNumTimes(str, num) {
+    if (num <= 0) {
+      return "";
+    } else {
+      return str += repeatStringNumTimes(str, num - 1); // WITHOUT USING THE REPEAT METHOD
+    }
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+  // TRUNCATE A STRING
+  function truncateString(str, num) {
+    return str.length > num ? str.slice(0,num) + "..." : str;
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+  // FINDERS KEEPERS
+  function findElement(arr, func) {
+    for (let i in arr) {
+      if (func(arr[i])) {
+        return arr[i];
+      }
+    }
+    return undefined;
+  }
+  
+  findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+  // BOO WHO
+  function booWho(bool) {
+    return typeof bool == "boolean" ? true : false;
+  }
+  
+  booWho(null);
+
+// TITLE CASE SENTENCE
+function titleCase(str) {
+    const arr = str.split(" ");
+    arr.map((w, i) => arr[i] = w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
+    return arr.join(" ");
+  }
+  
+  titleCase("I'm a little tea pot");
