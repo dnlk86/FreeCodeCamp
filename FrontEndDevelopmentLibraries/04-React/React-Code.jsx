cb91cb0e70f41fae8af55e786057931975f06b31
Daniel Keicher <dnlkch86@gmail.com>
2022-10-25 14:21:25 +0200

    added react section

---
+++ FrontEndDevelopmentLibraries/04-React/React-Code.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+const JSX = function () {
+    return (
+        <div>
+            <p>Paragraph One</p>
+            <p>Paragraph Two</p>
+            <p>Paragraph Three</p>
+        </div>
+    );
+};
+
+ReactDOM.render(JSX, document.getElementById("root"));
