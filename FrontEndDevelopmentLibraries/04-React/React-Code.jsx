8b18deb14eef0de7c0779c345ec2bff9a6b01723
Daniel Keicher <dnlkch86@gmail.com>
2022-10-30 12:03:42 +0100

    callbacks as props, lifecycle hooks

--- FrontEndDevelopmentLibraries/04-React/React-Code.jsx
+++ FrontEndDevelopmentLibraries/04-React/React-Code.jsx
@@ -49,3 +49,61 @@ class MyForm extends React.Component {
         );
     }
 }
+
+// Pass a callback as a prop
+class MyApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: "",
+        };
+        this.handleChange = this.handleChange.bind(this); // bindig "this"
+    }
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value,
+        });
+    }
+    render() {
+        return (
+            <div>
+                <GetInput
+                    input={this.state.inputValue}
+                    handleChange={this.handleChange} // passing down the handleChange function as a prop, it has access to state in the parent component
+                />
+                <RenderInput input={this.state.inputValue} />
+            </div>
+        );
+    }
+}
+
+class GetInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Get Input:</h3>
+                <input
+                    value={this.props.input}
+                    onChange={this.props.handleChange}
+                />
+            </div>
+        );
+    }
+}
+
+class RenderInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Input Render:</h3>
+                <p>{this.props.input}</p>
+            </div>
+        );
+    }
+}
